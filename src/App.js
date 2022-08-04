09201bd9f73c5a5c0f4c3ab72196f39ac62238d0
ANS <36783010+answebdev@users.noreply.github.com>
2022-08-03 19:05:25 -0700

    add toggle button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
@@ -6,15 +6,42 @@ import ExpenseList from './components/ExpenseList';
 import AddExpenseForm from './components/AddExpenseForm';
 import { AppProvider } from './context/AppContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 // Video: https://www.youtube.com/watch?v=aeYxBd1it7I
 // Code: https://github.com/chrisblakely01/react-budget-app
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleToggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <AppProvider>
-      <div className='container'>
-        <h1 className='mt-3'>Budget Tracker</h1>
+      <div className={`container ${darkMode && 'dark-mode'}`}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <div>
+            <h1 className='mt-3'>Budget Tracker</h1>
+          </div>
+          <div>
+            {' '}
+            <button
+              onClick={() => handleToggleDarkMode()}
+              // class='btn btn-primary'
+              className='toggle'
+            >
+              Toggle Mode
+            </button>
+          </div>
+        </div>
         <div className='row mt-3'>
           <div className='col-sm'>
             <Budget />
